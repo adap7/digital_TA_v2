630968c1fe56a97b9a531344a7bca36eb2ebe43c
adap7 <a.panghe@student.maastrichtuniversity.nl>
2026-01-28 15:17:35 +0100

    Added tests topic

---
+++ backend/courses/tests/test_topic_model.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from tenants.models import Tenant
+from courses.models import Course, Topic
+
+
+class TopicModelTest(TestCase):
+    def setUp(self):
+        self.tenant = Tenant.objects.create(name="Test University", slug="test-uni")
+        self.course = Course.objects.create(
+            tenant=self.tenant,
+            title="Calculus I",
+            code="CALC101",
+        )
+
+    def test_topic_creation(self):
+        topic = Topic.objects.create(
+            course=self.course,
+            title="Limits",
+            order_index=1,
+        )
+
+        self.assertEqual(topic.title, "Limits")
+        self.assertFalse(topic.is_published)
+        self.assertEqual(topic.course, self.course)
+
+    def test_topic_ordering(self):
+        Topic.objects.create(course=self.course, title="Derivatives", order_index=2)
+        Topic.objects.create(course=self.course, title="Limits", order_index=1)
+
+        topics = list(self.course.topics.all())
+        self.assertEqual(topics[0].title, "Limits")
+        self.assertEqual(topics[1].title, "Derivatives")
